b8cf15d66abbd908dd82258f81e17312ed80e41f
Dennis Hedback <d.hedback@gmail.com>
2026-01-15 09:16:37 +0100

    Add copilot

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,25 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'zbirenbaum/copilot.lua',
+    cmd = 'Copilot',
+    event = 'InsertEnter',
+    opts = {
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        keymap = {
+          accept = '<C-l>',
+          dismiss = '<C-k>',
+          next = '<C-j>',
+          prev = '<C-h>',
+        },
+      },
+      panel = {
+        enabled = false,
+      },
+    },
+  },
+}
